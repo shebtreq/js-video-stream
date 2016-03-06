23748c076f158759b3115ed8c88f3de261dc1a5e
shebtreq <shebtreq@github.com>
2016-03-06 18:03:07 +1100

    changed server.py

--- python-web-py/server.py
+++ python-web-py/server.py
@@ -1,12 +1,13 @@
 import web
 import serial
+import urllib2
 
-ser = serial.Serial()
-ser.baudrate = 9600
-ser.port = '/dev/ttyACM0'
-print ("Serial class", ser)
-ser.open()
-print ("Is serial port open?", ser.is_open)
+#ser = serial.Serial()
+#ser.baudrate = 9600
+#ser.port = '/dev/ttyACM0'
+#print ("Serial class", ser)
+#ser.open()
+#print ("Is serial port open?", ser.is_open)
 
 
 #Setup server
@@ -22,14 +23,16 @@ class server_default:
 
 class left:
     def GET(self, name):
-        ser.write(b'l');
-        print('left');
+#        ser.write(b'l')
+	urllib2.urlopen("http://192.168.0.100/command/ptzf.cgi?relative=0607").read()
+        print('left')
         return 'servo is going left'
 
 class right:
     def GET(self, name):
-        ser.write(b'r');
-        print('right');
+        #ser.write(b'r')
+	urllib2.urlopen("http://192.168.0.100/command/ptzf.cgi?relative=0407").read()
+        print('right')
         return 'servo is going right'
 
 if __name__ == "__main__":
